a097480f587ddb684ebd532544b03ec061850a27
Wangjiyuan <wangjiyuan102@163.com>
2021-01-03 23:50:02 +0800

    noise

---
+++ Wangjiyuan imnoise/imnoisegaussian.m
@@ -0,0 +1,19 @@
+x=result;%result 更改为变量数据名称
+R=real(x);
+[M,N,Y]=size(R);
+b=max(R(:));
+c=min(R(:));
+R=(R-c)/(b-c);
+g=imnoise(R,'gaussian',0,0.01);
+
+X=imag(x);
+[M,N,U]=size(X);
+d=max(X(:));
+e=min(X(:));
+X=(X-e)/(d-e);
+f=imnoise(X,'gaussian',0,0.01);
+T=(g*(b-c)+c)+((f*(d-e)+e)*1i);
+
+subplot(121),montage(reshape(abs(x),M,N,1,[]),'displayrange',[]),title('原图像');
+
+subplot(122),montage(reshape(abs(T),M,N,1,[]),'displayrange',[]),title('添加高斯噪声图像');
